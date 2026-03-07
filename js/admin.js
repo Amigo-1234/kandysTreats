a32e537efc1e0c2486c67c94d37046eaffc1178e
Amigo-1234 <olamidesamson841@gmail.com>
2026-03-07 14:25:28 +0100

    orderlink worked now

--- js/admin.js
+++ js/admin.js
@@ -497,7 +497,7 @@ const itemsSummary = order.subOrders
   .join("\n") || "• No items";
 
 // 🔗 Track link (CHANGE domain/path if needed)
-const trackLink = `https://kandystreats.com.ng/track.html?id=${order.id}`;
+const trackLink = `https://kandystreats.com.ng/track.html?id=${order.orderCode}`;
 
 const msg = `Hello ${order.customer.name} 👋
 
